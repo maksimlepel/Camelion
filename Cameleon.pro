7ad03bfe1ab4e2b083a2b6803c9ed000592a83b1
maksimlepel <maksimlepel@yandex.ru>
2023-04-06 11:41:16 +0300

    part2

---
+++ Cameleon.pro
@@ -0,0 +1,126 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-10-06T15:22:26
+#
+#-------------------------------------------------
+
+QT       += core gui network xml testlib\
+       multimedia multimediawidgets
+
+QT       += serialport
+
+#QT       += core dbus
+#CONFIG   -= app_bundle*/
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Cameleon
+TEMPLATE = app
+
+
+
+QMAKE_LIBDIR += C:/Qt/Projects/Cameleon_QT_BY/Cameleon/SDL/SDL-1.2.15/lib/x86
+
+Release:QMAKE_LIBDIR += C:/Qt/Projects/Cameleon/OgreSDK/lib/release
+
+
+
+# Edit below for custom library location
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    Dialogs/dgAudio.cpp \
+    Dialogs/dgLight.cpp \
+    Dialogs/dgLightSetting.cpp \
+    Joystick/v_joystick_adapter.cpp \
+    Thread/ControllerMsgSendThread.cpp \
+    Thread/ControllerMsgRcvThread.cpp \
+    Thread/SendingTimer.cpp \
+    Thread/ThreadPrototype.cpp \
+    Utils/ControllerState.cpp \
+    Utils/FuncUtils.cpp \
+    Utils/ShowMessage.cpp \
+    Thread/videothread.cpp \
+    Utils/RobotState.cpp \
+    OgreView/OgreView.cpp \
+    OgreView/modelloader.cpp \
+    Dialogs/dgCameraMode.cpp \
+    Thread/TabletBatteryLevelRequest.cpp \
+    Dialogs/CameraSelection.cpp \
+    Dialogs/dgArm.cpp \
+    Dialogs/dgVLC.cpp \
+    Utils/PingModel.cpp
+
+HEADERS  += mainwindow.h \
+    Dialogs/dgAudio.h \
+    Dialogs/dgLight.h \
+    Dialogs/dgLightSetting.h \
+    Dialogs/dgVideo.h \
+    Joystick/v_joystick_adapter.h \
+    Thread/ControllerMsgSendThread.h \
+    Thread/ControllerMsgRcvThread.h \
+    Thread/SendingTimer.h \
+    Thread/ThreadPrototype.h \
+    Utils/ConstInfo.h \
+    Utils/ControllerState.h \
+    Utils/FuncUtils.h \
+    Utils/ShowMessage.h \
+    Utils/RobotState.h \
+    Thread/videothread.h \
+    OgreView/OgreView.h \
+    OgreView/modelloader.h \
+    libvlc-qt/include/SharedExportCore.h \
+    libvlc-qt/include/SharedExportWidgets.h \
+    libvlc-qt/include/VideoDelegate.h \
+    Dialogs/dgCameraMode.h \
+    Utils/ModeTypes.h \
+    Thread/TabletBatteryLevelRequest.h \
+    Dialogs/CameraSelection.h \
+    Dialogs/dgArm.h \
+    Dialogs/dgVLC.h \
+    Utils/PingModel.h
+
+FORMS    += mainwindow.ui \
+    Dialogs/dgAudio.ui \
+    Dialogs/dglight.ui \
+    Dialogs/dgLightSetting.ui \
+    Dialogs/dgCameraMode.ui \
+    Dialogs/dgCameraSelection.ui \
+    Dialogs/dgArm.ui \
+    Dialogs/dgVLC.ui
+
+RESOURCES += \
+    resources.qrc
+
+win32: LIBS += -L$$PWD/../../../libvlc/lib/ -llibVLCQtCore.dll
+
+INCLUDEPATH += $$PWD/../../../libvlc/include
+DEPENDPATH += $$PWD/../../../libvlc/include
+
+win32: LIBS += -L$$PWD/../../../libvlc/lib/ -llibVLCQtWidgets.dll
+
+INCLUDEPATH += $$PWD/../../../libvlc/include
+DEPENDPATH += $$PWD/../../../libvlc/include
+
+win32: LIBS += -L$$PWD/SDL/SDL-1.2.15/lib/x86/ -lSDL
+
+INCLUDEPATH += $$PWD/SDL/SDL-1.2.15/lib/x86
+DEPENDPATH += $$PWD/SDL/SDL-1.2.15/lib/x86
+
+win32: LIBS += -L$$PWD/SDL/SDL-1.2.15/lib/x86/ -lSDLmain
+
+INCLUDEPATH += $$PWD/SDL/SDL-1.2.15/lib/x86
+DEPENDPATH += $$PWD/SDL/SDL-1.2.15/lib/x86
+
+
+INCLUDEPATH += $$PWD/SDL/SDL-1.2.15/include
+DEPENDPATH += $$PWD/SDL/SDL-1.2.15/include
+
+
+win32: LIBS += -L$$PWD/OgreSDK/lib/release/ -llibOgreMain.dll
+
+INCLUDEPATH += $$PWD/OgreSDK/lib/release
+DEPENDPATH += $$PWD/OgreSDK/lib/release
+
+INCLUDEPATH += $$PWD/OgreSDK/include
+DEPENDPATH += $$PWD/OgreSDK/include
---
+++ Cameleon.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Chameleon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="44"/>
+        <source>ВИДЕО</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <source>АУДИО</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>СВЕТ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="113"/>
+        <source>ИКСВЕТ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="136"/>
+        <source>КАМЕРА</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="159"/>
+        <source>ФОТО</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <source>УДАЛЕННО</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="205"/>
+        <source>ВЫСТРЕЛ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="228"/>
+        <source>РУКА</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="251"/>
+        <source>УПРАВЛЕНИЕ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="270"/>
+        <source>Башня+Плечо+ 
+ Локоть</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="290"/>
+        <source>Плечо+Локоть+ 
+ Запястье</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="310"/>
+        <source>Запястье+ 
+вращ.захв.+захв.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="334"/>
+        <source>ВЩКОП УЦЩ Щ Ц Щ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="371"/>
+        <source>ПРОЖЕКТОР</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="395"/>
+        <source>Кисть1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="438"/>
+        <source>Кисть2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="451"/>
+        <source>Задний</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="464"/>
+        <source>Передний</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="487"/>
+        <source>Локоть</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="640"/>
+        <source>Оконный режим</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="659"/>
+        <source>Быстро</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="678"/>
+        <source>Медленно</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="697"/>
+        <source>Стоп/Движение</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dgAudio</name>
+    <message>
+        <location filename="Dialogs/dgAudio.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgAudio.ui" line="34"/>
+        <source>Говорить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgAudio.ui" line="53"/>
+        <source>Слушать</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dgLight</name>
+    <message>
+        <location filename="Dialogs/dgLight.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLight.ui" line="34"/>
+        <source>Выключить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLight.ui" line="53"/>
+        <source>Передний</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLight.ui" line="72"/>
+        <source>Задний</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLight.ui" line="91"/>
+        <source>Кисть 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLight.ui" line="110"/>
+        <source>Кисть 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLight.ui" line="129"/>
+        <source>Локоть</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dgLightSetting</name>
+    <message>
+        <location filename="Dialogs/dgLightSetting.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLightSetting.ui" line="34"/>
+        <source>Выключить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLightSetting.ui" line="53"/>
+        <source>25%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLightSetting.ui" line="72"/>
+        <source>50%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLightSetting.ui" line="91"/>
+        <source>75%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgLightSetting.ui" line="110"/>
+        <source>100%</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dgVideo</name>
+    <message>
+        <location filename="Dialogs/dgVideo.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgVideo.ui" line="34"/>
+        <source>Видеозапись</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Dialogs/dgVideo.ui" line="53"/>
+        <source>Отложенный</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
